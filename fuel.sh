cc36a0d0e26aecad1a7fa5c2f69c0f67ce5096fc
Thomas <tlichten@users.noreply.github.com>
2016-05-23 16:01:45 +0200

    Wait for bootstrap image to download

--- fuel.sh
+++ fuel.sh
@@ -6,6 +6,12 @@ while ! wget -O /dev/null -o /dev/null --no-proxy 10.20.0.2:8000 &>/dev/null; do
 
 echo "Nailgun available"
 
+echo "Waiting for bootstrap image ..."
+
+while ! egrep "Bootstrap image (.*) has been activated" /var/log/fuel-bootstrap-image-build.log &>/dev/null; do :; sleep 5; echo '.'; done
+
+echo "Bootstrap image available"
+
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 sleep 120
