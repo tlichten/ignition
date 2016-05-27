cfbce02b897122272641b9365212d78a7ab3bbf7
Thomas <tlichten@users.noreply.github.com>
2016-05-27 13:32:28 +0200

    Update fuel.sh

--- fuel.sh
+++ fuel.sh
@@ -11,8 +11,15 @@ echo "Waiting for bootstrap image ..."
 while ! egrep "Bootstrap image (.*) has been activated" /var/log/fuel-bootstrap-image-build.log &>/dev/null; do :; sleep 5; echo '.'; done
 
 echo "Bootstrap image available"
+echo "Setting fuel admin password"
+PASSWORD=$(date +%s | sha256sum | base64 | head -c 8)
+sed -i 's/"password": "admin"/"password": "$PASSWORD"/' /etc/fuel/astute.yaml 
+sed -i "s/KEYSTONE_PASS: \"\(.*\)\"/KEYSTONE_PASS: \"$PASSWORD\"/" /root/.config/fuel/fuel_client.yaml 
+dockerctl shell astute service astute restart
 
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 sleep 120
+
+"Environment ready. Fuel admin password is $PASSWORD"
 
