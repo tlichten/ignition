c5cf356e8a751bb818a824f4c1b051548d465522
tlichten <tlichtenstein@mirantis.com>
2016-06-03 14:23:48 +0200

    Fixing quotes on sed password replacement

--- fuel.sh
+++ fuel.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 echo "Fuel starting up. Awaiting Nailgun server. This can take a few minutes ..."
 while ! wget -O /dev/null -o /dev/null --no-proxy 10.20.0.2:8000 &>/dev/null; do :; sleep 5; echo '.'; done
 
@@ -14,7 +13,7 @@ echo "Bootstrap image available"
 echo "Setting fuel admin password"
 PASSWORD=$(date +%s | sha256sum | base64 | head -c 8)
 fuel change-password --new-password=$PASSWORD
-sed -i 's/"password": "admin"/"password": "$PASSWORD"/' /etc/fuel/astute.yaml 
+sed -i "s/\"password\": \"admin\"/\"password\": \"$PASSWORD\"/" /etc/fuel/astute.yaml
 sed -i "s/KEYSTONE_PASS: \"\(.*\)\"/KEYSTONE_PASS: \"$PASSWORD\"/" /root/.config/fuel/fuel_client.yaml 
 dockerctl shell astute service astute restart
 
