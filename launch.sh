fbbf8a2f0cebd95ec3a55dfb084d88faf9003285
tlichten <tlichtenstein@mirantis.com>
2016-06-04 09:14:47 -0400

    Check for existence of vagran and plugins as well as iso before attemting install and download

--- launch.sh
+++ launch.sh
@@ -7,11 +7,11 @@ sh -c "echo 'options kvm-intel nested=y' >> /etc/modprobe.d/dist.conf"
 
 modprobe kvm-intel
 
-yum -y install https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.rpm
+rpm -qa | grep -qw vagrant || yum -y install https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.rpm
 
-vagrant plugin install vagrant-libvirt
+vagrant plugin list | grep -qw vagrant-libvirt || vagrant plugin install vagrant-libvirt
 
-wget http://9f2b43d3ab92f886c3f0-e8d43ffad23ec549234584e5c62a6e24.r60.cf1.rackcdn.com/MirantisOpenStack-8.0.iso -O /tmp/MirantisOpenStack-8.0.iso
+wget -N http://9f2b43d3ab92f886c3f0-e8d43ffad23ec549234584e5c62a6e24.r60.cf1.rackcdn.com/MirantisOpenStack-8.0.iso -O /tmp/MirantisOpenStack-8.0.iso
 chmod 777 /tmp/MirantisOpenStack-8.0.iso
 systemctl start libvirtd
 systemctl enable libvirtd
