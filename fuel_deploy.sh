0140baae78d7aebb6de270df482ed1a21ad2931f
tlichten <tlichtenstein@mirantis.com>
2016-06-09 16:03:25 -0400

    Added autodeploy of simple environment

--- fuel_deploy.sh
+++ fuel_deploy.sh
@@ -12,7 +12,7 @@ while [ $NUM_NODES_DISCOVERED -ne $NUM_NODES_EXPECTED ]; do
   echo "Discovered $NUM_NODES_DISCOVERED/$NUM_NODES_EXPECTED node(s) so far"
   sleep 5
 done
-
+PASSWORD= 
 if [ "$env_genpassword" = true ]; then
   echo "Setting fuel admin password"
   PASSWORD=$(date +%s | sha256sum | base64 | head -c 8)
@@ -28,4 +28,18 @@ do
  fuel node --node-id 00:$i --name fuelslave-$i 
 done
 
+fuel env create --name lab --rel 2 --net-segment-type vlan
+
+fuel node set --node 00:00 --role controller --env 1
+fuel node set --node 00:01 --role compute --env 1
+fuel node set --node 00:02 --role compute --env 1
+
+echo 'Starting deploy ...'
+fuel deploy-changes --env 1
 echo "Environment ready."
+if [ "$env_genpassword" = true ]; then
+  echo "Fuel admin password is $PASSWORD"
+fi
+echo "Horizon available at http://instance-public-ip."
+echo "Fuel available at https://instance-public-ip:8443"
+
