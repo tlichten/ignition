0c2ca87d08021203c725acf13fbd3c30ddf1d55d
tlichten <tlichtenstein@mirantis.com>
2016-08-12 14:18:38 +0000

    Enabling MOS 9.0

--- fuel.sh
+++ fuel.sh
@@ -17,7 +17,6 @@ echo "Setting default gw for slaves to 10.20.0.1"
 sed -i "s/\"dhcp_gateway\": \"10.20.0.2\"/\"dhcp_gateway\": \"10.20.0.1\"/" /etc/fuel/astute.yaml
 
 echo "Enable advanced and experimental features"
-sed -i "/\"FEATURE_GROUPS\":/a  - \"experimental\"\n- \"advanced\"" /etc/fuel/astute.yaml
 
 echo "Waiting for bootstrap image. This will take 20-30 minutes ..."
 
--- launch.sh
+++ launch.sh
@@ -13,12 +13,19 @@ rpm -qa | grep -qw vagrant || yum -y install https://releases.hashicorp.com/vagr
 vagrant plugin list | grep -qw vagrant-libvirt || vagrant plugin install vagrant-libvirt
 vagrant plugin list | grep -qw vagrant-triggers || vagrant plugin install vagrant-triggers
 
-wget -nc http://9f2b43d3ab92f886c3f0-e8d43ffad23ec549234584e5c62a6e24.r60.cf1.rackcdn.com/MirantisOpenStack-8.0.iso -O /tmp/MirantisOpenStack-8.0.iso
-chmod 777 /tmp/MirantisOpenStack-8.0.iso
+wget -nc http://9f2b43d3ab92f886c3f0-e8d43ffad23ec549234584e5c62a6e24.r60.cf1.rackcdn.com/MirantisOpenStack-9.0.iso -O /tmp/MirantisOpenStack.iso
+chmod 777 /tmp/MirantisOpenStack.iso
 systemctl start libvirtd
 systemctl enable libvirtd
 virsh net-define lib/vagrant-libvirt.xml
 virsh net-start vagrant-libvirt
+
+
+vagrant up &
+pid=$!
+while [ -d /proc/$pid ] ; do
+    virsh send-key ignition_fuelmaster KEY_F8
+done
 
 echo "Exposing installation on public interface"
 MYIP=$(curl -s 4.ifcfg.me)
@@ -26,10 +33,6 @@ iptables -I FORWARD -m state -d 10.20.0.0/24 --state NEW,RELATED,ESTABLISHED -j 
 iptables -t nat -I PREROUTING -p tcp -d $MYIP --dport 8443 -j DNAT --to-destination 10.20.0.2:8443
 iptables -I FORWARD -m state -d 172.16.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
 for i in {80,443,5000,6080,8000,8004,8080,8082,8386,8773,8774,8776,8777,9292,9696}; do iptables -t nat -I PREROUTING -p tcp -d $MYIP --dport $i -j DNAT --to-destination 172.16.0.3:$i; done
-
-
 iptables -t nat -A POSTROUTING -j MASQUERADE -s  172.16.0.0/24 ! -d 172.16.0.0/24
 iptables -t nat -A POSTROUTING -j MASQUERADE -s  10.20.0.0/24 ! -d 10.20.0.0/24
-
-vagrant up
 
